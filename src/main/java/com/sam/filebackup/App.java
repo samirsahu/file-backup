46a316ef6bbc90cbbefe822e51e97c51e7e407d7
Samir Sahu <samirsahu2k@yahoo.com>
2021-12-17 22:27:06 -0500

    added log4j. Started code.

--- src/main/java/com/sam/filebackup/App.java
+++ src/main/java/com/sam/filebackup/App.java
@@ -1,13 +1,28 @@
 package com.sam.filebackup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	static List<SyncDir> syncDirs = new ArrayList<>();
+	
+	static {
+		syncDirs.add(new SyncDir("C:\\Temp\\Sync1-1", "C:\\Temp\\Sync1-2"));
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		App me = new App();
+		me.start();
+	}
+	
+	private void start() {
+		for (SyncDir syncDir : syncDirs) {
+		
+		}
+	}
+	
 }
