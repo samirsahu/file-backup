47e90c54d39a591d9d361d0ce1f611382c2af39d
Samir Sahu <samirsahu2k@yahoo.com>
2021-12-30 16:33:50 -0500

    set to jdk 17

--- src/main/java/com/sam/filebackup/App.java
+++ src/main/java/com/sam/filebackup/App.java
@@ -1,5 +1,11 @@
 package com.sam.filebackup;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,22 +13,35 @@ import java.util.List;
  * Hello world!
  */
 public class App {
-	static List<SyncDir> syncDirs = new ArrayList<>();
-	
-	static {
-		syncDirs.add(new SyncDir("C:\\Temp\\Sync1-1", "C:\\Temp\\Sync1-2"));
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		App me = new App();
-		me.start();
-	}
-	
-	private void start() {
-		for (SyncDir syncDir : syncDirs) {
-		
-		}
-	}
-	
+    static List<SyncDir> syncDirs = new ArrayList<>();
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    static {
+        syncDirs.add(new SyncDir("C:\\Temp\\Sync1-1", "C:\\Temp\\Sync1-2"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        App me = new App();
+        me.start();
+    }
+
+    private void start() {
+        for (SyncDir syncDir : syncDirs) {
+
+            Path path1 = syncDir.getPath1();
+
+
+        }
+    }
+
+    private void parseDirectory(Path dirPath){
+        File dir = dirPath.toFile();
+        dir.isDirectory();
+        dir.isFile();
+
+
+    }
+
+
 }
--- src/main/java/com/sam/filebackup/SyncDir.java
+++ src/main/java/com/sam/filebackup/SyncDir.java
@@ -1,28 +1,30 @@
 package com.sam.filebackup;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SyncDir {
-	String path1;
-	String path2;
-	
-	public SyncDir(String path1, String path2) {
-		this.path1 = path1;
-		this.path2 = path2;
-	}
-	
-	public String getPath1() {
-		return path1;
-	}
-	
-	public void setPath1(String path1) {
-		this.path1 = path1;
-	}
-	
-	public String getPath2() {
-		return path2;
-	}
-	
-	public void setPath2(String path2) {
-		this.path2 = path2;
-	}
-	
+    Path path1;
+    Path path2;
+
+    public SyncDir(String path1, String path2) {
+        this.path1 = Paths.get(path1);
+        this.path2 = Paths.get(path2);
+    }
+
+    public Path getPath1() {
+        return path1;
+    }
+
+    public void setPath1(Path path1) {
+        this.path1 = path1;
+    }
+
+    public Path getPath2() {
+        return path2;
+    }
+
+    public void setPath2(Path path2) {
+        this.path2 = path2;
+    }
 }
